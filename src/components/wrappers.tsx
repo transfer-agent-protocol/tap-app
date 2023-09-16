22a57e9324dc7ae5c684e938211b914455131dbd
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-09-15 22:39:02 -0400

    Rework responsive components

--- src/components/wrappers.tsx
+++ src/components/wrappers.tsx
@@ -4,57 +4,45 @@ const FullWidth = styled.div`
     display: flex;
     flex-flow: column nowrap;
     justify-content: flex-start;
-    align-items: flex-start;
-    flex: 1;
-    width: auto;
-    min-height: 100vh;
-    padding: 0;
+	align-items: center;
+	width: 100%;
 `;
 
 const Nav = styled.nav`
     display: flex;
     flex-flow: row nowrap;
     justify-content: space-between;
-    align-items: center;
-    align-self: center;
-    width: 100%;
-    max-width: 768px;
-    height: 4rem;
-    padding: 0.5rem 0;
+	align-items: center;
+	width: 100%;
+	max-width: 768px;
 `;
 
 const Logotype = styled.span`
 	display: flex;
-	flex-flow: row nowrap;
+	flex-flow: row wrap;
 	justify-content: flex-start;
-	align-items: center;
-	width: 7.75rem;
+	align-items: flex-start;
 	text-align: left;
-	padding-left: 0.3rem;
+	font-size: ${({ theme }) => theme.fontSizes.baseline};
 	font-weight: 600;
+	width: 8rem
 `
 const Main = styled.main`
     display: flex;
     flex-flow: column nowrap;
-    align-items: flex-start;
-    align-self: center;
-    justify-content: flex-start;
-    flex: 1;
-    width: 100%;
-	max-width: 1400px;
-    height: auto;
-	padding: 0.5rem;
+	align-items: flex-start;
+	justify-content: flex-start;
+	width: 100%;
+	max-width: 768px;
+	padding: 0 0 0 3rem;
 
-    @media only screen and (max-width: 820px) {
-        width: 100%;
-    }
     /** Generic tablet and equivalent devices */
     @media only screen and (max-width: 768px) {
-        margin: 0;
     }
     /** iPhone portrait mode and equivalent devices */
     @media only screen and (max-width: 512px) {
-        margin: 0 auto;
+		width: 80%;
+		padding: 0 0 0 0.618rem;
     }
 
     & mark {
@@ -66,10 +54,10 @@ const Main = styled.main`
 const Heading = styled.div`
     display: flex;
     flex-flow: column nowrap;
-    align-items: center;
-    align-self: center;
-    text-align: center;
-    margin: 9rem 0 2.75rem 0;
+	align-items: flex-start;
+	justify-content: flex-start;
+	margin: 8rem 0 2rem 0;
+  
 
     /** iPhone portrait mode and equivalent devices */
     @media only screen and (max-width: 768px) {
@@ -85,23 +73,8 @@ const Heading = styled.div`
 const Content = styled.div`
     display: flex;
     flex-flow: column nowrap;
-    align-items: center;
-    align-self: center;
-    justify-content: flex-start;
-    flex: 1;
-    width: auto;
-    max-width: 1280px;
-    height: auto;
-    margin: 0 auto;
-
-    /** iPad Air and equivalent devices */
-    @media (max-width: 820px) {
-        width: 100%;
-    }
-    /** Generic tablet and equivalent devices */
-    @media (max-width: 768px) {
-        align-items: center;
-    }
+    align-items: flex-start;
+	justify-content: flex-start;
 `;
 
 const Article = styled.article`
@@ -132,6 +105,23 @@ const Article = styled.article`
     }
 `;
 
+const Credits = styled.div`
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: flex-start;
+	align-items: flex-start;
+	font-size: ${({ theme }) => theme.fontSizes.large};
+
+	a, span {
+        padding: 0 1rem 0 0;
+        /** iPhone portrait mode and equivalent devices */
+		@media only screen and (max-width: 512px) {
+			font-size: ${({ theme }) => theme.fontSizes.baseline};
+			padding: 0 0.3rem 0 0;
+		}
+    }
+`; 
+
 const TallyWrapper = styled.div`
 	position: relative;
     display: flex;
@@ -149,8 +139,6 @@ const FooterWrapper = styled.footer`
    	align-items: flext-start;
     align-self: center;
     justify-content: flex-start;
-    width: 46.80rem;
-	max-width: 1280px;
     margin: 8rem 0 4rem 0;
 `;
 
@@ -161,22 +149,6 @@ const FooterContent = styled.div`
 	align-items: flex-start;
 	width: 100%;
 	margin: 0 0 0 2.7rem;
-	
-
-    /** iPhone portrait mode and equivalent devices */
-    @media only screen and (max-width: 475px) {
-        font-size: ${({ theme }) => theme.fontSizes.medium};
-    }
-
-    a, span {
-        font-size: ${({ theme }) => theme.fontSizes.large};
-        padding: 0 1rem 0 0;;
-
-        /** iPad portrait mode and equivalent devices */
-        @media only screen and (max-width: 768px) {
-            font-size: ${({ theme }) => theme.fontSizes.large};
-        }
-    }
 `;
 
 const FooterAside = styled.aside`
@@ -202,4 +174,4 @@ const FooterAside = styled.aside`
     }
 `;
 
-export { FullWidth, Nav, Logotype, Main, Heading, Content, Article, TallyWrapper, FooterWrapper, FooterContent, FooterAside };
+export { FullWidth, Nav, Logotype, Main, Heading, Content, Article, Credits, TallyWrapper, FooterWrapper, FooterContent, FooterAside };
