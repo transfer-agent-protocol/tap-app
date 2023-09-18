9a005dd24a55af153411214150bd3c06fd1f9629
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-09-18 16:47:28 -0400

    Adds links to github and docs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,12 +10,17 @@ export default function Navbar() {
 				<Link href="/">
 					<Image src="/tap-glyph.svg" alt="Transfer Agent Protocol" width={48} height={48} />
 				</Link>
-				<Logotype>Transfer Agent Protocol</Logotype>
+				<Logotype>Transfer Agent Protocol_</Logotype>
 			</LogoRouter>
 			<span>
 				<StyledA>
-					<Link href="https://github.com/poet-network/tap-cap-table">
+					<Link href="https://github.com/poet-network/tap-cap-table" target="_blank ">
 						<Image src="/icons/github.svg" alt="TAP Cap Table Github" width={32} height={32} />
+					</Link>
+				</StyledA>
+				<StyledA>
+					<Link href="https://coda.io/d/Transfer-Agent-Protocol_drhpwRhDok-/Transfer-Agent-Protocol_sua17#_lu_Fh" target="_blank">
+						<Image src="/icons/docs.svg" alt="Documentation" width={32} height={32} />
 					</Link>
 				</StyledA>
 			</span>
