4f57271b467152adc4f8311635af0b4f749df41c
Alex Palmer <24211477+ThatAlexPalmer@users.noreply.github.com>
2023-09-18 16:49:05 -0400

    Update copy

--- src/pages/join.tsx
+++ src/pages/join.tsx
@@ -1,22 +1,19 @@
 import Head from "next/head";
 import { Content, Heading } from "../components/wrappers";
-import { H1, Blockquote, P } from "../components/typography";
+import { H1, Blockquote } from "../components/typography";
 
 export default function Join() {
 	return (
 		<Content>
 			<Head>
-				<title>Join</title>
+				<title>Join Us</title>
 			</Head>
 			<Heading>
-				<H1>There is nothing here yet.</H1>
+				<H1>Join our 2nd founding cohort of builders</H1>
 			</Heading>
 			<Blockquote>
-				And Liberty for All
+				Read our <a href="https://coda.io/d/Transfer-Agent-Protocol_drhpwRhDok-/Transfer-Agent-Protocol_sua17#_lu_Fh">docs</a>, run our <a href="https://github.com/poet-network/tap-cap-table">code</a>, tell us what brought you here. We'd love your help improving this stack and infra.
 			</Blockquote>
-			<P>
-				- they said
-			</P>
 		</Content>
 	);
 }
